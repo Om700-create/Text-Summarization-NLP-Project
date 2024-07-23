f6cc4d6a56fe2c1519988a5338487424cc74233e
Om700-create <om.data07@gmail.com>
2024-07-23 13:24:10 +0545

    model train

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from textSummarizer.logging import logger
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -34,3 +36,14 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
